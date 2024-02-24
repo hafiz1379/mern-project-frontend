0362e5f0075c7fb49d96fbd5b4217f7f4c719d4d
hafiz1379 <hafizrasa1379@gmail.com>
2024-02-24 22:30:28 +0430

    Remove unused imports

--- src/App.js
+++ src/App.js
@@ -6,11 +6,6 @@ import {
   Switch,
 } from "react-router-dom";
 
-// import Users from './user/pages/Users';
-// import NewPlace from './places/pages/NewPlace';
-// import UserPlaces from './places/pages/UserPlaces';
-// import UpdatePlace from './places/pages/UpdatePlace';
-// import Auth from './user/pages/Auth';
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import { AuthContext } from "./shared/context/auth-context";
 import { useAuth } from "./shared/hooks/auth-hook";
